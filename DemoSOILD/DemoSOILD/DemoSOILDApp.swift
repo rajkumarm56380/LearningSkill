5d6d8394ab96fe814036d52beceb344ae07b7def
user <user@users-iMac-Pro-25.local>
2026-02-27 17:58:25 +0530

    add files

---
+++ DemoSOILD/DemoSOILD/DemoSOILDApp.swift
@@ -0,0 +1,17 @@
+//
+//  DemoSOILDApp.swift
+//  DemoSOILD
+//
+//  Created by user on 27/02/26.
+//
+
+import SwiftUI
+
+@main
+struct DemoSOILDApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
